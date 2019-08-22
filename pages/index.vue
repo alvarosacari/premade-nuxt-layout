984d26ba8c2107272506055e31d74fe72479c75d
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-08-22 11:37:43 -0500

    card actions has been removed

--- pages/index.vue
+++ pages/index.vue
@@ -8,16 +8,6 @@
         <v-card-text>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab possimus quibusdam sed recusandae tempora a perferendis nostrum. Esse impedit ducimus quas aliquam cumque ut laudantium culpa odio. In, consectetur esse.</p>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
