20d8fb9aebc3f3e25ef62c65cdaae438259839f7
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-08-22 11:35:57 -0500

    vuetify theme has been updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,16 +60,25 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
+        light: {
+          primary: colors.blue,
+          accent: colors.orange,
+          secondary: colors.blue.lighten2,
+          success: colors.green,
+          info: colors.lightBlue,
+          warning: colors.amber,
+          error: colors.red
+        },
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          primary: colors.blue,
+          accent: colors.orange,
+          secondary: colors.blue.lighten2,
+          success: colors.green,
+          info: colors.lightBlue,
+          warning: colors.amber,
+          error: colors.red
         }
       }
     }
