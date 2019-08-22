47815718a70733b10b80067aa4e9a827abb78852
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-08-22 12:29:18 -0500

    Footer component has been created

---
+++ components/default_layout/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-footer
+    color="primary"
+    :inset="!inset"
+    dark
+    app
+  >
+    <span>
+      Premade Layout © {{ year }} Developed by
+      <a
+        class="white--text"
+        :href="github.url"
+        target="_blank"
+      >{{ github.user }}</a>
+    </span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  props: {
+    inset: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+      github: {
+        url: 'https://github.com/alvarosacari',
+        user: 'Alvaro Sacari'
+      }
+    }
+  },
+
+  computed: {
+    year () {
+      return new Date().getFullYear()
+    }
+  }
+}
+</script>
