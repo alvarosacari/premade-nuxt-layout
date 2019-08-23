63d89a93ec068f30615845892bff2d10b1ad61b8
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-08-23 13:04:19 -0500

    default layout has been updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,7 @@
-import { NuxtError } from '../.nuxt';
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-    </v-app-bar>
+    <NavigationDrawer v-model="layout" />
+    <AppBar v-model="layout" :title="title" />
 
     <v-content>
       <v-container fluid>
@@ -42,35 +9,26 @@ import { NuxtError } from '../.nuxt';
       </v-container>
     </v-content>
 
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
+    <Footer :inset="layout.clipped" />
   </v-app>
 </template>
 
 <script>
+import Footer from '~/components/default_layout/Footer.vue'
+import NavigationDrawer from '~/components/default_layout/NavigationDrawer.vue'
+import AppBar from '~/components/default_layout/AppBar.vue'
+
 export default {
+  components: {
+    Footer, NavigationDrawer, AppBar
+  },
   data () {
     return {
-      clipped: true,
-      drawer: true,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
+      layout: {
+        clipped: true,
+        drawer: true,
+        miniVariant: false
+      },
       title: 'Premade Layout'
     }
   }
