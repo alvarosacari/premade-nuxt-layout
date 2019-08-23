24070323a15582cf623f8c1761365047b8172628
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-08-23 13:03:03 -0500

    app bar has been created

---
+++ components/default_layout/AppBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar
+    dark
+    color="primary"
+    :clipped-left="value.clipped"
+    fixed
+    app
+  >
+    <v-app-bar-nav-icon @click.stop="value.drawer = !value.drawer" />
+    <v-toolbar-title v-text="title" />
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Object,
+      default () { return {} }
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
